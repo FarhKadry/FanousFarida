f7175b9a20818ca7ff6140a50edbbd7095fa7834
Farh Kadry <23-101289@students.eui.edu.eg>
2026-06-03 15:11:31 +0300

    loading page done

--- fanous/src/pages/loading.jsx
+++ fanous/src/pages/loading.jsx
@@ -1,39 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './home.css'
 import './levels.css'
 import './loading.css'
-
 import './../animations.css'
 import './../components/layout/header.css'
-import top from './../assets/logo/top.svg'
-import bottom from './../assets/logo/bottom.svg'
-import fanous from './../assets/logo/fanous.svg'
-import depth from './../assets/depth2.svg'
-
-import farida from './../assets/logo/farida.svg'
-import starz from './../assets/logo/starz 2.svg'
-import menu from './../assets/menu.svg'
-
-import starshine from './../assets/logo/star shine.svg'
-import character from './../assets/charsplash2.png'
 
 import splash from './../assets/splashbg.jpg'
-import { Link } from 'react-router-dom';
-import Button from '../components/common/button';
-import IconBtn from '../components/common/iconbtn';
+import { useNavigate } from 'react-router-dom';
 
 const Loading = () => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            navigate('/home');
+        }, 5000);
+
+        return () => clearTimeout(timer);
+    }, [navigate]);
+
     return ( <>
-    <div className="fixed-mobile-wrapper">
-        <img className='splashBg bg2' src={splash} alt="" />
-       <div className="splashBg depth depth2"></div>
-       <div className="loadingFanous">
-        
-       </div>
-        {/* <img src={logo} alt='' /> */}
-        
-    </div>
+        <div className="fixed-mobile-wrapper">
+            <img className='splashBg bg2' src={splash} alt="" />
+            <div className="splashBg depth depth2"></div>
+            <div className="loadingFanous"></div>
+            <h2 className='flicker'> جارٍ التحميل </h2>
+        </div>
     </> );
 }
- 
+
 export default Loading;

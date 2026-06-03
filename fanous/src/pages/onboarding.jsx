418bf65a0e16098ccc00ccb4b9b379e9b1448d63
Farh Kadry <23-101289@students.eui.edu.eg>
2026-06-03 21:39:12 +0300

    styled onboarding page

--- fanous/src/pages/onboarding.jsx
+++ fanous/src/pages/onboarding.jsx
@@ -2,21 +2,15 @@ import React from 'react';
 import './home.css'
 import './../animations.css'
 import './../components/layout/header.css'
-import top from './../assets/logo/top.svg'
-import bottom from './../assets/logo/bottom.svg'
-import fanous from './../assets/logo/fanous.svg'
-import depth from './../assets/depth2.svg'
 
-import farida from './../assets/logo/farida.svg'
-import starz from './../assets/logo/starz 2.svg'
+import depth from './../assets/onboardDepth.svg'
+
 import menu from './../assets/menu.svg'
 
-import starshine from './../assets/logo/star shine.svg'
 import character from './../assets/onboardchar.png'
 
 
 import splash from './../assets/onboardbg.png'
-import { Link } from 'react-router-dom';
 import Button from '../components/common/button';
 import IconBtn from '../components/common/iconbtn';
 import TapAnimation from '../components/common/tap';
@@ -33,10 +27,10 @@ const Onboarding = () => {
         <img className='splashBg ' src={splash} alt="" />
         <img className='splashBg' src={depth} alt="" />
         {/* <img src={logo} alt='' /> */}
+        <h2 className='onboardText flicker'> اضغط الشاشة  للبدأ! </h2>
         <TapAnimation />
-        <h2> اضغط الشاشة  للبدأ! </h2>
-        <img className='charSplash' src={character} alt="" />
-        <div className="startBtnCont startBtnAnim">
+        <img className='floatIn onboardChar' src={character} alt="" />
+        <div className="missonBtn">
         <Button link="/gameplay1" style1="primarybtn " cta="مهمتي " />
         </div>
     </div>

9beb6a5a28c66df67362fc0d186bab6f0407277a
Farh Kadry <23-101289@students.eui.edu.eg>
2026-06-12 02:30:57 +0300

    created mosque animation

--- fanous/src/pages/prewin.jsx
+++ fanous/src/pages/prewin.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './home.css'
 import './../animations.css'
+import './prewin.css'
+
 import './../components/layout/header.css'
 import { useNavigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 
 import depth from './../assets/onboardDepth.svg'
+import fanous from './../assets/fanous_empty.png';
+import pause from './../assets/pause.svg';
 
 import menu from './../assets/menu.svg'
 
@@ -15,53 +19,40 @@ import character2 from './../assets/onboardchar.png'
 
 
 
-import splash from './../assets/onboardbg.png'
+import splash from './../assets/mosque1bg.jpg'
 import Button from '../components/common/button';
 import IconBtn from '../components/common/iconbtn';
 import TapAnimation from '../components/common/tap';
+import Music from '../components/common/music';
+import Progress from '../components/common/progress';
 
 const PreWin = () => {
-       const navigate = useNavigate();
-    const [started, setStarted] = useState(false);
-
-    const handleStart = () => {
-        setStarted(true);
-    };
-
-    useEffect(() => {
-        if (!started) return;
-        const timer = setTimeout(() => {
-            navigate('/gameplay1');
-        }, null);
-        return () => clearTimeout(timer);
-    }, [started, navigate]);
+    const starsCollected = parseInt(localStorage.getItem('lastStarsCollected') ?? '0', 10);
 
     return ( <>
-     <div onClick={handleStart} className="fixed-mobile-wrapper">
-             {!started && (
-                            <div className="tapOverlay" onClick={handleStart}>
-                            اضغط للبدء
-                            </div>
-                        )}
-        <header>
-    <IconBtn
-        icon={menu}
-        style1="iconbtnmian"
-        link="/menu" />
-        </header>
-        <img className='splashBg ' src={splash} alt="" />
+     <div className="fixed-mobile-wrapper">
+            
+       <header>
+                <div className="flex2">
+                    <IconBtn icon={pause} style1="iconbtnmian" link="/pause" />
+                    <Music />
+                </div>
+                <Progress counter={starsCollected} fanous={fanous} />
+            </header>
+        <img className='splashBg mosqueBg mosque1Anim' src={splash} alt="" />
         <img className='splashBg' src={depth} alt="" />
-       <div className='floatIn'>
-         <h2 style={{ animationDelay: '0.1s' }} className='onboardText flicker'> اضغط الشاشة  للبدأ! </h2>
-        <TapAnimation />
-       </div>
-        <img  style={{ animationDelay: '0.2s' }} className='floatIn onboardChar' src={character} alt="" />
+       <div className="floatIn narration prewinNarration">
+            لقد اقتربنا من ابن طولون  
+
+          <div className="startBtnCont">
+        </div>
+        </div>
+        <img className='float prewinChar' src={character} alt="" />
         <div
     style={{ animationDelay: '0.3s', zIndex: '999999999' }}
     className="missonBtn floatIn"
     onClick={(e) => e.stopPropagation()}
 >
-    <Button link="/mission" style1="primarybtn" cta="مهمتي" />
 </div>
     </div>
     </> );
